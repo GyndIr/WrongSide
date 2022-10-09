d9f96f7c3b2856d926984560651d84c023595ae9
Alexandr <drk91@inbox.ru>
2022-10-09 22:17:34 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root "chydak#Vtapke"
   get 'chydak/Vtapke'
   get "/chydak", to: "chydak#Vtapke"
-  get "/chydak/:id", to: "chydak#show"
 
   resources :chydak do
     resources :comments
